8da2a79275be45538bc613f27e2a8e7d7a06dd80
nguyenjessev <nguyenjessev@gmail.com>
2021-11-25 13:00:10 -0500

    Add flashes for creating/updating/deleting kittens

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -11,8 +11,10 @@ class KittensController < ApplicationController
     @kitten = Kitten.new(kitten_params)
 
     if @kitten.save
+      flash.notice = 'Kitten successfully created!'
       redirect_to @kitten
     else
+      flash.alert = 'An error occurred!'
       render :new
     end
   end
@@ -29,8 +31,10 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
 
     if @kitten.update(kitten_params)
+      flash.notice = 'Kitten successfully updated!'
       redirect_to @kitten
     else
+      flash.alert = 'Error updating kitten!'
       render :edit
     end
   end
@@ -39,6 +43,7 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
     @kitten.destroy
 
+    flash.notice = 'Kitten successfully deleted!'
     redirect_to root_path
   end
 end
